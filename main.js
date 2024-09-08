4bf5e47c5b638b308c85b0a5aac8665bcf13d18c
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-09-08 11:21:21 +0800

    editing adding

--- main.js
+++ main.js
@@ -28,6 +28,28 @@ app.get ('/recordings', async (req, res)=> {
 
 })
 
+app.get ('/types/:typename/:categoryname', async (req, res)=> {
+  const typename = req.params.typename
+  const categoryname = req.params.categoryname
+  let result = sql `select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id where 1=1 `
+  if (typename) {
+     result = `${result} and allsrecord.alltransactiontypes=${typename}`
+  }
+  if (categoryname) {
+    result = `${result} and allsrecord.categoryid=${categoryname}`
+ }
+ const list = await result
+ console.log(list)
+  res.json (list)
+ 
+})
+
+// app.get ('/types/:typename', async (req, res)=> {
+//   const typename = req.params.typename
+//   const result = await sql `select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id where  allsrecord.alltransactiontypes = ${typename} `
+//   res.json (result)
+// })
+
  app.post('/recordings', async (req, res) => {
   const id = uuidv4()
   const {alltype, amount, category, date, time, payee, note} =req.body
@@ -42,17 +64,18 @@ app.delete('/recordings/:id', async (req, res) => {
   res.sendStatus(204)
 })
 
-app.put ('/categories/:id', async (req, res) => {
-  const {id} =req.params
-  const {name} =req.body
-  const result =  await sql`update  allcategories set name = ${name} where id = ${id} `
+app.put ('/recordings/:id', async (req, res) => {
+  const idedited =req.params.id
+  const {alltype, category, amount,date,time,payee,note} =req.body
+  console.log(idedited, alltype,category,amount,date,time,payee,note)
+  const result =  await sql`update  allsrecord set alltransactiontypes=${alltype},amount=${amount}, date=${date}, time=${time}, payee=${payee}, note=${note}, categoryid=${category} where id = ${idedited} `
   res.sendStatus(204)
 })
 
-app.get ('/income', async (req, res)=> {
-  const result = await sql `select SUM (amount) from allsrecord GROUP BY typesall`
-  res.json (result)
-})
+// app.get ('/income', async (req, res)=> {
+//   const result = await sql `select SUM (amount) from allsrecord GROUP BY typesall`
+//   res.json (result)
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
