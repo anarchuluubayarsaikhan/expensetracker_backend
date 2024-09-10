fcd60dcd888f1e5da52474551538a0de0eeb5ffb
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-09-10 18:16:47 +0800

    filter adding

--- main.js
+++ main.js
@@ -2,46 +2,51 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const {sql} = require ('./config/database')
+const { sql } = require('./config/database')
 const { v4: uuidv4 } = require('uuid')
 const port = 4000
 
-app.use(cors()) 
+app.use(cors())
 app.use(express.json())
 
 
-app.get ('/categories', async (req, res)=> {
-  const result = await sql `select * from allcategories`
-  res.json (result)
+app.get('/categories', async (req, res) => {
+  const result = await sql`select * from allcategories`
+  res.json(result)
 })
 
- app.post('/categories', async (req, res) => {
+app.post('/categories', async (req, res) => {
   const id = uuidv4()
-  const {name,color, icon} =req.body
-  const result =  await sql`insert into allcategories (id, name, color, icon) values (${id}, ${name},${color}, ${icon})`
+  const { name, color, icon } = req.body
+  const result = await sql`insert into allcategories (id, name, color, icon) values (${id}, ${name},${color}, ${icon})`
   res.sendStatus(204)
 })
 
-app.get ('/recordings', async (req, res)=> {
-  const result = await sql `select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id `
-  res.json (result)
+app.get('/recordings', async (req, res) => {
+  const result = await sql`select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id `
+  res.json(result)
 
 })
 
-app.get ('/types/:typename/:categoryname', async (req, res)=> {
+app.get('/types/:typename/:categoryname', async (req, res) => {
   const typename = req.params.typename
   const categoryname = req.params.categoryname
-  let result = sql `select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id where 1=1 `
+  let result = sql`select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id where 1=1 `
+
+  // console.log("result", result);
+
   if (typename) {
-     result = `${result} and allsrecord.alltransactiontypes=${typename}`
+    result = sql`${result} and allsrecord.alltransactiontypes=${typename}`
   }
+  
   if (categoryname) {
-    result = `${result} and allsrecord.categoryid=${categoryname}`
- }
- const list = await result
- console.log(list)
-  res.json (list)
- 
+    result = sql`${result} and allsrecord.categoryid=${categoryname}`
+  }
+
+  console.log(result.raw)
+  const list = await result; 
+  console.log(list)
+  res.json(list)
 })
 
 // app.get ('/types/:typename', async (req, res)=> {
@@ -50,25 +55,25 @@ app.get ('/types/:typename/:categoryname', async (req, res)=> {
 //   res.json (result)
 // })
 
- app.post('/recordings', async (req, res) => {
+app.post('/recordings', async (req, res) => {
   const id = uuidv4()
-  const {alltype, amount, category, date, time, payee, note} =req.body
-  const result =  await sql`insert into allsrecord (id, categoryid, alltransactiontypes, amount, date, time, payee, note) values (${id}, ${category},${alltype}, ${amount}, ${date}, ${time}, ${payee},${note})`
+  const { alltype, amount, category, date, time, payee, note } = req.body
+  const result = await sql`insert into allsrecord (id, categoryid, alltransactiontypes, amount, date, time, payee, note) values (${id}, ${category},${alltype}, ${amount}, ${date}, ${time}, ${payee},${note})`
   res.sendStatus(204)
 })
 
 app.delete('/recordings/:id', async (req, res) => {
-  const {id} =req.params
-  console.log (req.params)
-  const result =  await sql`delete from allsrecord where id = ${id} `
+  const { id } = req.params
+  console.log(req.params)
+  const result = await sql`delete from allsrecord where id = ${id} `
   res.sendStatus(204)
 })
 
-app.put ('/recordings/:id', async (req, res) => {
-  const idedited =req.params.id
-  const {alltype, category, amount,date,time,payee,note} =req.body
-  console.log(idedited, alltype,category,amount,date,time,payee,note)
-  const result =  await sql`update  allsrecord set alltransactiontypes=${alltype},amount=${amount}, date=${date}, time=${time}, payee=${payee}, note=${note}, categoryid=${category} where id = ${idedited} `
+app.put('/recordings/:id', async (req, res) => {
+  const idedited = req.params.id
+  const { alltype, category, amount, date, time, payee, note } = req.body
+  console.log(idedited, alltype, category, amount, date, time, payee, note)
+  const result = await sql`update  allsrecord set alltransactiontypes=${alltype},amount=${amount}, date=${date}, time=${time}, payee=${payee}, note=${note}, categoryid=${category} where id = ${idedited} `
   res.sendStatus(204)
 })
 
