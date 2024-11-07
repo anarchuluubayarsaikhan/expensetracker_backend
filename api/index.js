440d67560c9180f4780943135fd61517c4186367
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-11-07 16:20:02 +0800

    first commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const { sql } = require('./config/database')
+const { sql } = require('../config/database')
 const { v4: uuidv4 } = require('uuid')
 const port = 4000
 
@@ -10,25 +10,30 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/categories', async (req, res) => {
+app.get('expense-backend-umber.vercel.app/categories', async (req, res) => {
   const result = await sql`select * from allcategories`
   res.json(result)
 })
 
-app.post('/categories', async (req, res) => {
+app.get('expense-backend-umber.vercel.app/categories', async (req, res) => {
+  const result = await sql`select * from allcategories`
+  res.json(result)
+})
+
+app.post('expense-backend-umber.vercel.app/categories', async (req, res) => {
   const id = uuidv4()
   const { name, color, icon } = req.body
   const result = await sql`insert into allcategories (id, name, color, icon) values (${id}, ${name},${color}, ${icon})`
   res.sendStatus(204)
 })
 
-app.get('/recordings', async (req, res) => {
+app.get('expense-backend-umber.vercel.app/recordings', async (req, res) => {
   const result = await sql`select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id `
   res.json(result)
 
 })
 
-app.get('/types', async (req, res) => {
+app.get('expense-backend-umber.vercel.app/types', async (req, res) => {
   const { typename, categoryname ,daterange} = req.query
   console.log(req.query)
   let result = sql`select allsrecord.id, allsrecord.alltransactiontypes, allsrecord.amount, allsrecord.date, allsrecord.time, allsrecord.payee, allsrecord.note, allcategories.name, allcategories.icon, allcategories.color  from  allsrecord left join  allcategories  on allsrecord.categoryid = allcategories.id where 1=1 `
@@ -52,20 +57,20 @@ app.get('/types', async (req, res) => {
 
 )
 
-app.post('/recordings', async (req, res) => {
+app.post('expense-backend-umber.vercel.app/recordings', async (req, res) => {
   const id = uuidv4()
   const { alltype, amount, category, date, time, payee, note } = req.body
   const result = await sql`insert into allsrecord (id, categoryid, alltransactiontypes, amount, date, time, payee, note) values (${id}, ${category},${alltype}, ${amount}, ${date}, ${time}, ${payee},${note})`
   res.sendStatus(204)
 })
 
-app.delete('/recordings/:id', async (req, res) => {
+app.delete('expense-backend-umber.vercel.app/recordings/:id', async (req, res) => {
   const { id } = req.params
   const result = await sql`delete from allsrecord where id = ${id} `
   res.sendStatus(204)
 })
 
-app.put('/recordings/:id', async (req, res) => {
+app.put('expense-backend-umber.vercel.app/recordings/:id', async (req, res) => {
   const idedited = req.params.id
   const { alltype, category, amount, date, time, payee, note } = req.body
   const result = await sql`update  allsrecord set alltransactiontypes=${alltype},amount=${amount}, date=${date}, time=${time}, payee=${payee}, note=${note}, categoryid=${category} where id = ${idedited} `
